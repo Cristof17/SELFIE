247ebe423a96c8ee6f9ccc69ebb55f5d17723450
Cristof17 <cristof_rotsching@yahoo.com>
2014-12-04 02:34:13 +0200

    RawImage Component implemented superficially

--- src/components/RawPhoto.java
+++ src/components/RawPhoto.java
@@ -13,7 +13,20 @@ public class RawPhoto extends Component{
 	@Override
 	public Message notify(Message message) {
 		MessageImage messageImage = (MessageImage)message ;
-		return null;
+		
+		int[][][] pixels = messageImage.getPixels();
+		int width = messageImage.getWidth();
+		int height = messageImage.getHeight();
+		
+		int[][][] rotatedPixels = new int[height][width][3];
+		for(int i = 0 ; i < height ; i ++ ){
+			for(int j = 0 ; j < width ; j ++){
+				rotatedPixels[height-i-1][width -j -i] = pixels[i][j];
+			}
+		}
+		
+		Message resulted = new MessageImage(TaskType.IMAGE_SAVE,rotatedPixels , width ,height);
+		return resulted;
 	}
 
 }
